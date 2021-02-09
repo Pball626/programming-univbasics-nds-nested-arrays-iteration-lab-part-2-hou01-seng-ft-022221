38b856fc70e759c19fed62d1bb29d8aacfb31902
Jose Mendoza <pball626@gmail.com>
2021-02-09 14:41:18 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,25 +1,27 @@
-require "pry"
-def find_min_in_nested_arrays(src)
-  # src will be an array of arrays of integers
-  # Produce a new Array that contains the smallest number of each of the nested arrays
 
-outer_results = []
-row_index = 0  
-  while row_index < src.length do
-    daily_lows = []
-    element_index = 0 
+
+# require "pry"
+# def find_min_in_nested_arrays(src)
+#   # src will be an array of arrays of integers
+#   # Produce a new Array that contains the smallest number of each of the nested arrays
+
+# outer_results = []
+# row_index = 0  
+#   while row_index < src.length do
+#     daily_lows = []
+#     element_index = 0 
     
-    while element_index < src[row_index].length do
-      row_index = 0 
-      if src[row_index][element_index] == src[row_index].min 
-        daily_lows = src[row_index][element_index]
-      binding.pry
-      end
-      element_index += 1 
-    end
-    weekly_lows_array << daily_lows
-    row_index += 1 
-  end
-  weekly_lows_array
+#     while element_index < src[row_index].length do
+#       row_index = 0 
+#       if src[row_index][element_index] == src[row_index].min 
+#         daily_lows = src[row_index][element_index]
+#       binding.pry
+#       end
+#       element_index += 1 
+#     end
+#     weekly_lows_array << daily_lows
+#     row_index += 1 
+#   end
+#   weekly_lows_array
   
-end
+# end
