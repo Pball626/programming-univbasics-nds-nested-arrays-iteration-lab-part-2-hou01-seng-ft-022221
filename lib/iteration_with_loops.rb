cfea9cf94ad8a58e3ebb5e812379d2a94a1f9e4b
Jose Mendoza <pball626@gmail.com>
2021-02-09 14:31:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def find_min_in_nested_arrays(src)
 
 outer_results = []
 row_index = 0  
-  while row_index < src.count do
+  while row_index < src.length do
     daily_lows = []
     element_index = 0 
     
